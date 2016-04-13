e7d79081a7d9ce01b067406897ea9c126db024ac
Alex <1989alpan@gmail.com>
2016-04-13 16:28:34 +0300

    First commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def contacts
+  end
+
+  def authentication
+  end
+
+end
---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+
+  describe "GET #home" do
+    it "returns http success" do
+      get :home
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #contacts" do
+    it "returns http success" do
+      get :contacts
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
