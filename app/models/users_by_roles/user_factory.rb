9daa72789ebfabcd67c3bddd9a63a73a6070c0ba
Quicks <1989alpan@gmail.com>
2016-05-24 08:50:57 +0300

    Finish user factory

--- app/models/users_by_roles/user_factory.rb
+++ app/models/users_by_roles/user_factory.rb
@@ -3,13 +3,14 @@ class UsersByRoles::UserFactory
   def self.make_user(user_params)
     # factory for user
     # create user by roles
-    user = User.new
+    user = User.new(user_params)
     case user_params[:roles]
       when 'customer'
-        binding.pry
         user = UsersByRoles::Customer.new(user_params)
       when 'performer'
         user = UsersByRoles::Performer.new(user_params)
+      else
+        raise 'Can not find user roles'
     end
     user
   end
