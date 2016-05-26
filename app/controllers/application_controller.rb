10df7641e19a7d8b428ac4d96587157a651fa731
Quicks <1989alpan@gmail.com>
2016-05-26 22:45:01 +0300

    finish redirect to user profile by roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :set_cabinet_url
+
+  def set_cabinet_url
+    @cabinet_url = set_profile_path_by_user_role(current_user)
+  end
+
   protected
 
   def redirect_to_back(default = root_url)
@@ -33,21 +39,18 @@ class ApplicationController < ActionController::Base
     # if user is performer redirect to performer path
     # else if user is customer redirect to customer path(now we going to root path,because i don't realize that scenario)
     # else if user is admin redirect to admin path(now we going to root path,because i don't realize that scenario)
-    # by default redirect to root_path
-
-    if user.is_performer?
-      module_performer_show_path(user.nick_name)
-    elsif user.is_customer?
-      root_path
-    elsif user.is_admin?
-      root_path
-    else
-      root_path
+    # by default redirect to nil
+    unless user.nil?
+      if user.is_performer?
+        module_performer_show_path(user.nick_name)
+      elsif user.is_customer?
+        root_path
+      elsif user.is_admin?
+        root_path
+      else
+        nil
+      end
     end
-  private
-
-
-
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_user
 
-  def show
+  def check_user
     @user = User.get_user_by_nick_name(params[:nick]).first
     unless @user.eql? current_user
       redirect_to_back
