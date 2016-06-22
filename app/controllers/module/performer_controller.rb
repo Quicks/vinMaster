22f97ad4feaa80ee5e6890777f948b3475e9b916
Quicks <1989alpan@gmail.com>
2016-06-22 21:34:10 +0300

    Add tender comment modal window

--- app/controllers/module/performer_controller.rb
+++ app/controllers/module/performer_controller.rb
@@ -1,4 +1,15 @@
 class Module::PerformerController < UsersController
+  before_action :check_access
+  before_action :check_user,only: [:show]
+
   def show
+  end
+
+  private
+
+  def check_access
+    unless current_user.is_performer?
+      redirect_to :back
+    end
   end
 end
