c89cfe79026775a2666b664e49dc978db30ed652
Quicks <1989alpan@gmail.com>
2016-07-13 20:31:48 +0300

    add_markers_spr1

--- app/controllers/geojson_controller.rb
+++ app/controllers/geojson_controller.rb
@@ -1,17 +1,29 @@
 class GeojsonController < ApplicationController
   def get_tenders
-    result = {
-        "type": "Feature",
-        "properties": {
-            "name": "Coors Field",
-            "amenity": "Baseball Stadium",
-            "popupContent": "This is where the Rockies play!"
-        },
-        "geometry": {
-            "type": "Point",
-            "coordinates": [31.2,48.5]
-        }
-    }.to_json
+    result = []
+    tenders = Module::CustomerManage::Tender.all.entries
+    tenders.each do |tender|
+      result << {
+          type: 'Feature',
+          properties: {
+              title: tender.title
+
+          }
+
+      }
+    end
+    # result = {
+    #     "type": "Feature",
+    #     "properties": {
+    #         "name": "Coors Field",
+    #         "amenity": "Baseball Stadium",
+    #         "popupContent": "This is where the Rockies play!"
+    #     },
+    #     "geometry": {
+    #         "type": "Point",
+    #         "coordinates": [31.2,48.5]
+    #     }
+    # }.to_json
 
     respond_to do |format|
       format.json { render json: result }
