ec3c542f26bba538102b89e9dc5c5cd644ce8859
Quicks <1989alpan@gmail.com>
2016-08-07 15:42:37 +0300

    Add leaflet to project

--- app/controllers/geojson_controller.rb
+++ app/controllers/geojson_controller.rb
@@ -1,32 +1,31 @@
 class GeojsonController < ApplicationController
   def get_tenders
     result = []
+
     tenders = Module::CustomerManage::Tender.all.entries
     tenders.each do |tender|
-      result << {
-          type: 'Feature',
-          properties: {
-              title: tender.title
-
-          }
-
-      }
+      unless tender[:coordinates].blank? || tender[:coordinates][0].nil? || tender[:coordinates][1].nil?
+        result << {
+            type: 'Feature',
+            properties: {
+                title: tender.title,
+                category: tender.category.to_s,
+                id: tender.id.to_s
+            },
+            geometry:{
+                type: 'Point',
+                coordinates: tender.coordinates.reverse!
+            }
+        }
+      end
     end
-    # result = {
-    #     "type": "Feature",
-    #     "properties": {
-    #         "name": "Coors Field",
-    #         "amenity": "Baseball Stadium",
-    #         "popupContent": "This is where the Rockies play!"
-    #     },
-    #     "geometry": {
-    #         "type": "Point",
-    #         "coordinates": [31.2,48.5]
-    #     }
-    # }.to_json
 
+    geo_json = {
+        'type' => 'Feature Collection',
+        'features' => result
+    }
     respond_to do |format|
-      format.json { render json: result }
+      format.json { render json: geo_json }
     end
   end
 end
